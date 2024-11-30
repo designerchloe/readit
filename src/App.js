c8580210a3ea888a0559712aed159a83d89799e5
designerChloe <chloe.allard72@gmail.com>
2024-11-29 23:30:17 -0500

    Add navbar with searchbar and logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Posts from './features/post/posts';
 import Genre from './components/genre';
+import Searchbar from './components/searchbar';
+import Logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="page-container">
+      <nav>
+        <img className="logo" src={Logo} />
+        <Searchbar />
+      </nav>
       <h1>Let's Talk Books...</h1>
       <div className="content-container" >
         <Posts />
